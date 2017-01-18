c6ec452ffdb1d9af8b9c23ab07db95fd6a63b6a7
Bernie Perez <bernie@atomtickets.com>
2017-01-18 00:08:19 -0800

    Okay the real initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Alexa = require('alexa-sdk'),
+    APP_ID = "amzn1.ask.skill.2503b285-f3cb-43ce-91cf-820e23bcb60c";
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('AboutPapaBear');
+    },
+    'GetAboutPapaBearIntent': function () {
+        this.emit('AboutPapaBear');
+    },
+    'AboutPapaBear': function () {
+    var speechOutput = "Papa Bear is a Yorkie from Los Angeles! Saying goes, \"His bark is worst than his bite.\" But they haven't met him yet! RAWR";
+    var cardTitle = "About Papa Bear";
+    this.emit(':tellWithCard', speechOutput, cardTitle, speechOutput)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can say tell me about Papa Bear, or, you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
